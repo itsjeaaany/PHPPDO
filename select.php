7e81114d6b5c9098a5dd9cdce0c1b38d136b534c
Jeany Anne Butalid <j.butalid.553781@umindanao.edu.ph>
2026-03-03 18:02:37 +0800

    Update select.php

--- select.php
+++ select.php
@@ -1,9 +1,11 @@
 <?php
 require 'config.php';
 
-$stmt = $pdo->query("SELECT * FROM users");
+$stmt = $pdo->query("SELECT u.users_id, u.name, u.email, o.orders_id, o.product, o.amount
+    FROM users u
+    LEFT JOIN orders o ON u.users_id = o.users_id"
+);
+
+
 $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-
 ?>
