c56743e6fd57b00f0afab3fa092b36b48923322f
Jeany Anne Butalid <j.butalid.553781@umindanao.edu.ph>
2026-03-03 18:01:40 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
 $host = "localhost";
-$dbname = "pdo_crud";
+$dbname = "pdo";
 $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname",
+     $username, $password);
+    $pdo ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
